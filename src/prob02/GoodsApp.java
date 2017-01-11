92543c98e54e6ada14ab95908d19ba0009870b53
T3AD20 <T3AD20@G209-30>
2017-01-11 15:44:23 +0900

    17/01/11

--- src/prob02/GoodsApp.java
+++ src/prob02/GoodsApp.java
@@ -3,28 +3,51 @@ package prob02;
 import java.util.Scanner;
 
 public class GoodsApp {
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		Goods goods = new Goods();
+//		String[] name1 = new String[3];
+//		int[] price1 = new int[3];
+//		int[] countStock1 = new int[3];
+//		
+//		for(int i = 0; i < 3; i++) {
+//			String name = scanner.next();
+//			int price = scanner.nextInt();
+//			int countStock = scanner.nextInt();
+//			name1[i]=name;
+//			price1[i]=price;
+//			countStock1[i]=countStock;
+////			goods.setName(name);
+////			goods.setPrice(price);
+////			goods.setCountStock(countStock);
+//		}
+//		for( int i = 0; i < 3; i++ ) {
+//			System.out.println(name1[i]+price1[i]+countStock1[i]);
+////			goods.show();
+//		}
+//		scanner.close();
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Goods goods = new Goods();
-		String[] name1 = new String[3];
-		int[] price1 = new int[3];
-		int[] countStock1 = new int[3];
+		Goods[] goods = new Goods[3];
 		
 		for(int i = 0; i < 3; i++) {
 			String name = scanner.next();
 			int price = scanner.nextInt();
 			int countStock = scanner.nextInt();
-			name1[i]=name;
-			price1[i]=price;
-			countStock1[i]=countStock;
-//			goods.setName(name);
-//			goods.setPrice(price);
-//			goods.setCountStock(countStock);
+			goods[ i ] = new Goods();
+			goods[i].setName(name);
+			goods[i].setPrice(price);
+			goods[i].setCountStock(countStock);
 		}
-		for( int i = 0; i < 3; i++ ) {
-			System.out.println(name1[i]+price1[i]+countStock1[i]);
-//			goods.show();
+		System.out.println(goods[0].getName());
+		for( Goods g : goods ) {
+			System.out.println( 
+				g.getName() +
+				"(가격:" + g.getPrice() + ")이 " +
+				g.getCountStock() + "개 입고 되었습니다." );
 		}
+			
+		
 		scanner.close();
 	}
 }
